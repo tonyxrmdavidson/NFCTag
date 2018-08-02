eb8239221af5e299f3401db583db3a84395e9550
tonyxrmdavidson <tonyxrmdavidson@yahoo.co.uk>
2018-08-02 15:07:01 +0100

    [TD] Adding Mocha/Chai testing framework

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+//var server = require('../lib/server.js');
+var request = require('request');
+var assert = require('chai').assert;
+
+describe('NFCTag', function () {
+	
+});
